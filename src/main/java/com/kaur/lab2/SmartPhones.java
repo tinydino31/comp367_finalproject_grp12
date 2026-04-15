13672aee78b45d3e8102c0fc971a766c94c5a4b6
Jashanjot Kaur <jkau1146@my.centennialcollege.ca>
2026-04-14 22:04:47 -0400

    Configured via Spring XML for loose coupling

--- src/main/java/com/kaur/lab2/SmartPhones.java
+++ src/main/java/com/kaur/lab2/SmartPhones.java
@@ -5,7 +5,8 @@ package com.kaur.lab2;
  * Week 2 Lab - Constructor dependency injection using XML configuration
  */
 public class SmartPhones implements Products {
-
+	// Service layer bean implementation
+	// Configured via Spring XML for loose coupling
     @Override
     public void printMessage() {
         System.out.println("I am a Smart Phone");
