4efdd1c531f9832188ca229299e7a9bcd0b9755d
Jashanjot Kaur <jkau1146@my.centennialcollege.ca>
2026-04-19 19:51:34 -0400

    Add unit tests for basic functionality and addition

---
+++ src/main/java/com/kaur/lab2/AppTest.java
@@ -0,0 +1,17 @@
+package com.kaur.lab2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testBasic() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
